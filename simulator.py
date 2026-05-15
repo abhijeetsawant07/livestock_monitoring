53d9800752be528c987b41d0140aba00c67b4e17
Dipali Mandalik <dipali@Dipalis-MacBook-Air.local>
2026-05-14 20:12:39 -0500

    improve simulator for cloud

--- simulator.py
+++ simulator.py
@@ -5,33 +5,43 @@ import time
 URL = "https://livestock-monitoring.onrender.com/sensor-data"
 #URL = "http://localhost:5001/sensor-data"
 GOATS = ["goat_1", "goat_2", "goat_3"]
+print("...Simulator started...")
 
 while True:
     for goat in GOATS:
         # Simulate different behavior per goat
-        if goat == "goat_1":
-            temp = round(random.uniform(38, 39), 2)
-            movement = random.randint(10, 20)
-            feed = random.randint(300, 500)
+        try:
+            # --- Simulate behavior ---
+            if goat == "goat_1":
+                temp = round(random.uniform(38, 39), 2)
+                movement = random.randint(10, 20)
+                feed = random.randint(300, 500)
 
-        elif goat == "goat_2":
-            temp = round(random.uniform(39, 41), 2)  # slightly high
-            movement = random.randint(0, 5)           # low movement
-            feed = random.randint(100, 200)
+            elif goat == "goat_2":
+                temp = round(random.uniform(39, 41), 2)
+                movement = random.randint(0, 5)
+                feed = random.randint(100, 200)
 
-        else:  # goat_3
-            temp = round(random.uniform(38, 40), 2)
-            movement = random.randint(5, 15)
-            feed = random.randint(150, 350)
+            else:  # goat_3
+                temp = round(random.uniform(38, 40), 2)
+                movement = random.randint(5, 15)
+                feed = random.randint(150, 350)
 
-        data = {
-            "goat_id": goat,
-            "temperature": temp,
-            "movement": movement,
-            "feed": feed
-        }
+            data = {
+                "goat_id": goat,
+                "temperature": temp,
+                "movement": movement,
+                "feed": feed
+            }
 
-        requests.post(URL, json=data)
-        print("Sent:", data)
+            response = requests.post(URL, json=data, timeout=5)
+
+            if response.status_code == 200:
+                print("Data Sent:", data)
+            else:
+                print("Failed:", response.text)
+
+        except Exception as e:
+            print("Error sending data:", str(e))
 
     time.sleep(5)
